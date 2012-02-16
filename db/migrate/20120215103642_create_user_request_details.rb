c143ad01f3189083e2c2b40cf71321dd8726abbe
Surendrans <surendrans86@gmail.com>
2012-02-16 14:54:09 +0530

    added todo list and changed status bool to string

--- db/migrate/20120215103642_create_user_request_details.rb
+++ db/migrate/20120215103642_create_user_request_details.rb
@@ -3,7 +3,7 @@ class CreateUserRequestDetails < ActiveRecord::Migration
     create_table :user_request_details do |t|
       t.integer :sender_id
       t.integer :receiver_id
-      t.boolean :status
+      t.string :status		#sent, accepted, rejected
       t.datetime :request_sent_on
       t.datetime :request_confirmed_on
 
